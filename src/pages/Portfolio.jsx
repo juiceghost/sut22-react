c2c66199a56a2ae2250db7b106f2ebb38f911ba5
Kristian Kjeldsen <jg@renegade.se>
2023-03-07 15:32:37 +0100

    added some stuff

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -2,22 +2,23 @@ import React, { useState, useEffect } from "react";
 import axios from 'axios';
 
 function Portfolio() {
-    const [data, setData] = useState({ work: [] });
+    const [data, setData] = useState({ repos: [] });
     const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
         const fetchData = async () => {
             setIsLoading(true);
 
-            const result = await axios('cv.json');
+            const result = await axios('https://api.github.com/users/juiceghost/repos');
             console.log(result.data)
-            setData(result.data);
+            setData({ repos: result.data });
             setIsLoading(false);
         };
 
         fetchData();
     }, []);
     console.log("Hej från Portfolio")
+
     return (
         <>
             <h1>Portfolio</h1>
@@ -25,10 +26,10 @@ function Portfolio() {
                 <div>Loading ...</div>
             ) : (
                 <ul>
-                    {data.work.map(item => (
-                        <li>
+                    {data.repos.map(item => (
+                        <li key={item.id}>
                             <h4>{item.name}</h4>
-                            <p>{item.role}</p>
+                            <p>{item.html_url}</p>
                             <br></br>
                         </li>
                     ))}
