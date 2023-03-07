e5d6d1a180c9723d6afb4001deebbdc60cd4f3b5
Kristian Kjeldsen <jg@renegade.se>
2023-03-07 11:21:51 +0100

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
 import { useState } from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import reactLogo from './assets/react.svg'
 import './App.css'
 
@@ -6,28 +12,36 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Router>
+      <div className="grid-container">
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/portfolio">Portfolio</Link>
+          <Link to="/contact">Contact</Link>
+          <Link to="/about">About</Link>
+        </nav>
+        <main>
+          <p className="read-the-docs">
+            Click on the Vite and React logos to learn more
+          </p>
+          <Switch>
+            <Route path="/about">
+              <h1>About</h1>
+            </Route>
+            <Route path="/contact">
+              <h1>Contact</h1>
+            </Route>
+            <Route path="/portfolio">
+              <h1>Portfolio</h1>
+            </Route>
+            <Route path="/">
+              <h1>Main</h1>
+            </Route>
+          </Switch>
+        </main>
+        <footer>(C) Krille</footer>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    </Router>
   )
 }
 
